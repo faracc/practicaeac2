e4152aed57282113b5addc259915b8f18a8cb815
isard <isard@debian>
2026-03-26 20:05:22 +0100

    actualització eac2

--- djproject1/settings.py
+++ djproject1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ojao-8(+-^l_w=ht92xms_t7%u!6@^us1xv*be$+(#3uhipc#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
